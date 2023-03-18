81ee6dd5d6387717e3bbe452f15365475feb36bc
ehhhahah <ehhhahah@gmail.com>
2023-03-18 11:42:10 +0100

    some fixes?

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import { Route, Routes } from 'react-router-dom'
 import MainPage from './MainPage'
-import NoMatch from './NoMatch'
 
 export default function App() {
-  return (
-    <div>
-      <Routes>
-        <Route index path='/' element={<MainPage />} />
-        <Route path='*' element={<NoMatch />} />
-      </Routes>
-    </div>
-  )
+  return <MainPage />
 }
--- src/NoMatch.js
+++
@@ -1,9 +0,0 @@
-function NoMatch() {
-  return (
-    <div>
-      <h1>404 - No match!</h1>
-    </div>
-  )
-}
-
-export default NoMatch
